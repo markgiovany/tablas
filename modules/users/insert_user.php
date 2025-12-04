85c042f7951b1006480618106e718711b36e1b7e
maoriab <maorimichell788@gmail.com>
2025-12-03 20:23:18 -0500

    Observación
    Al agregar un usuario no debería dar opcion de colocarlo como activo/inactivo

--- modules/users/insert_user.php
+++ modules/users/insert_user.php
@@ -7,9 +7,8 @@ $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
 $password = $_POST['password'];
-$status = $_POST['status'];
 
-$query = "INSERT INTO users (name, phone, email, password, status) VALUES ('$name', '$phone', '$email', '$password', '$status')";
+$query = "INSERT INTO users (name, phone, email, password) VALUES ('$name', '$phone', '$email', '$password', '$status')";
 $conexion->query($query);
 header('Location:./')
 ?>
